310343beb04e3b23c86127690da89e8b3f1e8ea0
Clark Du <clark.duxin@gmail.com>
2018-09-26 21:28:05 +0100

    feat: detectESVersion and detectCodes

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const acorn = require('acorn')
 const glob = require('glob')
 const { readFileSync } = require('fs')
 
+const ECMA_VERSIONS = [10, 9, 8, 8, 6, 5]
+
 module.exports = class Detector {
   readSource (fileName) {
     return readFileSync(fileName, 'utf-8')
@@ -11,14 +13,31 @@ module.exports = class Detector {
     try {
       acorn.parse(source, { ecmaVersion })
     } catch (err) {
-      return { compatible: false, msg: err.message }
+      return { esVersion: 0, msg: err.message }
     }
-    return { compatible: true }
+    return { esVersion: ecmaVersion }
+  }
+
+  detectESVersion (source) {
+    for (const version of ECMA_VERSIONS) {
+      const result = this.detect(source, version)
+      if (result.esVersion !== 0) {
+        return result
+      }
+    }
+    return { esVersion: 0, msg: 'No ecmaScript version is found in given resource' }
+  }
+
+  detectCodes (source, ecmaVersion) {
+    if (ecmaVersion === undefined) {
+      return this.detectESVersion(source)
+    }
+    return this.detect(source, ecmaVersion)
   }
 
   detectFile (fileName, ecmaVersion) {
     const source = this.readSource(fileName)
-    return this.detect(source, ecmaVersion)
+    return this.detectCodes(source, ecmaVersion)
   }
 
   detectFiles (pattern, ecmaVersion) {
